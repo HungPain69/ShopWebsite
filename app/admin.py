2be295a59bae5a4a5db949e5b1c38738b829fb95
hungpain <hungpain69@gmail.com>
2018-10-21 02:46:29 +0700

    create branch and database

---
+++ app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Logins)
+admin.site.register(Customer)
+admin.site.register(Orders)
+admin.site.register(DeliveryAddresses)
+admin.site.register(OderItems)
+admin.site.register(Product)
+admin.site.register(Categories)
+admin.site.register(Admins)
